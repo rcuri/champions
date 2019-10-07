8d4d79edfdfd69cedf38f4cf02f07ca7795a64bc
Rodrigo Curiel <rodrigocuriel95@gmail.com>
2019-10-06 19:59:33 -0700

    ready for deploy

--- comparenba/config.py
+++ comparenba/config.py
@@ -3,7 +3,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'SECRET_KEY'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-thang-right-hurr'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     'postgresql+psycopg2://postgres:@localhost/champion'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    ELASTICSEARCH_URL = 'http://localhost:9200'
